244bf62d51b0b2e587f47ec111885f4ae9704a62
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-07-17 19:08:30 -0400

    Update activity.tsx

--- pages/activity.tsx
+++ pages/activity.tsx
@@ -46,10 +46,10 @@ export default function ActivityPage() {
   return (
     <Layout>
       <Head>
-        <title>Passliss</title>
+        <title>Iftiin-Pass</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/smol.ico" />
       </Head>
       <PageContent page="activity">
         <div className="mb-2 flex items-center space-x-2">
